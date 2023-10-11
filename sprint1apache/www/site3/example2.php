1f51bfd55c2745489176f2527ad7488c1bf354dc
Natasha Rey Arotkina <natashareyarotkina@gmail.com>
2023-10-11 15:23:54 +0200

    Ejercicio2, lo subo ahora por problemas que tuve el fin de semana

---
+++ sprint1apache/www/site3/example2.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+	<h1>Página de bienvenidas</h1>
+<?php
+	function dar_bienvenida($nombre) {
+	echo "¡Bienvenida, " . $nombre . "!";
+}
+dar_bienvenida("Lisa");
+?>
+</body>
+</html>
