e02d81c94770a66139723a7a512d3e0c0407138c
LilMoonstar <145000695+LilMoonstar@users.noreply.github.com>
2023-10-18 13:45:21 +0200

    Cambio de estilos css

--- sprint2phpsql/main.php
+++ sprint2phpsql/main.php
@@ -1,66 +1,65 @@
 <?php
-//Establecer la conexión a la base de datos
-	$db = mysqli_connect('localhost','root','1234','mysitedb') or die('Fail');
+// Establecer la conexión a la base de datos
+$db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail');
 
-//Lanzar una consulta
-	$query = 'SELECT * FROM tJuegos';
-	$result = mysqli_query($db,$query) or die('Error en la consulta');
-
-//HTML para mostrar los datos
+// Lanzar una consulta
+$query = 'SELECT * FROM tJuegos';
+$result = mysqli_query($db, $query) or die('Error en la consulta');
 ?>
 
 <!DOCTYPE html>
-	<html>
-	<head>
-		<title>Comentarios</title>
-		<style>
-			.comment {
-				border: 1px solid #ccc;
-				margin: 10px;
-				padding: 10px;
-				}
-			img{
-				max-width: 500px;
-				max-height: 5000px;
-				}
-			body{
-				font-family: "Roboto", sans-serif;
-			}
+<html>
+<head>
+    <title>Juegos</title>
+    <style>
+        body {
+            font-family: "Roboto", sans-serif;
+            background-color: #363232;
+            color: #000;
+        }
 
-			h1{
+        h1 {
+            font-weight: bold;
+            text-align: center;
+            color: #cc472b;
+        }
 
-			font-family: "Roboto", sans-serif;
-			font-weight: bold;
-			text-align: center;
-			}
+        .comment {
+            border: 1px solid #fff;
+            margin: 10px;
+            padding: 10px;
+            background-color: #fff;
+            color: #000;
+        }
 
-		</style>
-	</head>
+        img {
+            max-width: 500px;
+            max-height: 5000px;
+        }
+    </style>
+</head>
+
 <body>
-	<h1>Juegos</h1>
-	<?php
-	//Recorrer el resultado
-	while ($row = mysqli_fetch_array($result)) {
-		echo '<div class="comment">';
-		echo '<p>Nombre: '.$row[1]. '</p>';
-		echo '<p>Año de lanzamiento: '.$row[3]. '</p>';
-		echo '<p>Categoría: '.$row[4]. '</p>';
-
-	//enlace para ver más detalles
-		echo '<a href="/detail.php?id=' .$row['id']. '">Ver detalles</a> <br>';	
-	//Mostrar la imagen
-		echo '<img src="'.$row[2].'"alt="Imagen del juego">';
-		echo '</div>';
-	}
-	?>
+    <h1>Juegos</h1>
+    <?php
+    // Recorrer el resultado
+    while ($row = mysqli_fetch_array($result)) {
+        echo '<div class="comment">';
+        echo '<p>Nombre: ' . $row[1] . '</p>';
+        echo '<p>Año de lanzamiento: ' . $row[3] . '</p>';
+        echo '<p>Categoría: ' . $row[4] . '</p>';
+        // Enlace para ver más detalles
+        echo '<a href="/detail.php?id=' . $row['id'] . '">Ver detalles</a> <br>';
+        // Mostrar la imagen
+        echo '<img src="' . $row[2] . '" alt="Imagen del juego">';
+        echo '</div>';
+    }
+    ?>
 </body>
 </html>
 
 <?php
-	//cerrar la conexion a la bd
-	msqli_close($db);
+// Cerrar la conexión a la base de datos
+mysqli_close($db);
 ?>
-
-
-
 <h1>Conexión establecida</h1>
