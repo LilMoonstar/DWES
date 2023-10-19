18dd2e667d8da459844c869ae596749c13e607f5
Natasha Rey Arotkina <natashareyarotkina@gmail.com>
2023-10-19 12:10:02 +0200

    Problemitas con el numero de comentario

--- sprint2phpsql/comentario.php
+++ sprint2phpsql/comentario.php
@@ -25,18 +25,23 @@
 </head>
 <body>
 <?php
+$db = mysqli_connect('localhost','root','1234','mysitedb')or die('Fail');
+
 // Verifica si se ha establecido la variable success en la URL
-if (isset($_GET['success']) && $_GET['success'] == 1) {
-    echo 'Comentario creado con éxito.';
+if (isset($_GET['success']) && isset($_GET['comment_id'])) {
+	$comment_id = $_GET['comment_id'];
+    echo 'Comentario número '.$comment_id.' creado con éxito.';
 } else {
     echo 'Hubo un problema al crear el comentario.';
 }
 
 // Agrega un botón para volver a detail.php
-echo '<div class="center">';
-echo '<img src="https://i.pinimg.com/474x/cb/ca/3b/cbca3b17684b9e153771ee6013261f4e.jpg" alt="Imagen" style="max-width: 500px; max-height: 500px;">';
-echo '<div class="button"><a href="detail.php?id=' . $_GET['juego_id'] . '">Volver</a></div>';
-echo '</div>';
+if (isset($_GET['juego_id'])) {
+	echo '<div class="center">';
+	echo '<img src="https://i.pinimg.com/474x/cb/ca/3b/cbca3b17684b9e153771ee6013261f4e.jpg" alt="Imagen" style="max-width: 500px; max-height: 500px;">';
+	echo '<div class="button"><a href="detail.php?id=' . $_GET['juego_id'] . '">Volver</a></div>';
+	echo '</div>';
+}
 ?>
 </body>
 </html>
