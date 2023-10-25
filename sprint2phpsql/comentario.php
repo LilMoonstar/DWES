f78282a0e0c5d27a6df8d559cba638e6a0e16713
LilMoonstar <145000695+LilMoonstar@users.noreply.github.com>
2023-10-25 22:04:29 +0200

    Ejercicio 5 comentario.php

--- sprint2phpsql/comentario.php
+++ sprint2phpsql/comentario.php
@@ -1,42 +1,41 @@
-<!DOCTYPE html>
+<?php
+$db = mysqli_connect('localhost','root','1234','mysitedb')or die('Fail');
+?>
 <html>
 <head>
     <style>
         body {
-            background-color: #cfe2f3; /* Color de fondo azul clarito */
+            background-color: #cfe2f3; 
             font-family: "Roboto", sans-serif;
             text-align: center;
             font-size: 40px;
             font-weight: bold;
-            text-transform: uppercase;
             color: black;
-        }
-
-        .center {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-        }
-
-        .button {
-            margin-top: 20px; /* Espacio entre la imagen y el botón */
         }
     </style>
 </head>
 <body>
-<?php
-// Verifica si se ha establecido la variable success en la URL
-if (isset($_GET['success']) && $_GET['success'] == 1) {
-    echo 'Comentario creado con éxito.';
-} else {
-    echo 'Hubo un problema al crear el comentario.';
-}
 
-// Agrega un botón para volver a detail.php
-echo '<div class="center">';
-echo '<img src="https://i.pinimg.com/474x/cb/ca/3b/cbca3b17684b9e153771ee6013261f4e.jpg" alt="Imagen" style="max-width: 500px; max-height: 500px;">';
-echo '<div class="button"><a href="detail.php?id=' . $_GET['juego_id'] . '">Volver</a></div>';
-echo '</div>';
+<?php
+
+// Carga de variables
+
+$juego_id=$_POST['juego_id'];
+$comentario=$_POST['new_comment'];
+
+// Inserción del contenido del comentario en la tabla de SQL
+
+$query = "INSERT INTO tComentarios (comentario,usuario_id,juego_id) VALUES ('$comentario',NULL,'$juego_id')";
+mysqli_query($db,$query) or die('Error de creacion');
+
+//Texto de la página
+
+echo "<p>Nuevo comentario número ".mysqli_insert_id($db)." añadido con éxito</p>";
+echo "<a href='/detail.php?juego_id=".$juego_id."'>Volver a los comentarios</a>";
+
+// Cerrar la base de datos
+mysqli_close($db);
+
 ?>
 </body>
 </html>
