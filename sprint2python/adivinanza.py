84682f2d83d7f37666e7b5198e4d770066c44f7f
LilMoonstar <nreya22@fpcoruna.afundacion.org>
2023-11-04 19:57:25 +0100

    Ejercicio 1 Adivinanza

---
+++ sprint2python/adivinanza.py
@@ -0,0 +1,9 @@
+print("Adivinanza")
+print("Escoge la opción correcta")
+print("Tengo agujas y no sé coser, tengo números y no sé leer, ¿qué soy?")
+print("A un reloj    B Una patata   C Una mezcladora de cemento")
+respuesta = input("¿Cuál es la respuesta correcta?")
+if respuesta == "A":
+    print("Correcto!")
+else:
+    print("Incorrecto")
